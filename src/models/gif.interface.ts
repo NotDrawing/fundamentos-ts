35315a0b628e6064efe38f24f1a06f9d18b9c56c
Urbina Gutierrez Angel <113421459+NotDrawing@users.noreply.github.com>
2026-09-22 07:44:13 -0700

    feat: configurar modelos y acceso a GIPHY API

--- src/models/gif.interface.ts
+++ src/models/gif.interface.ts
@@ -1,10 +1,10 @@
 export type GifRating = 'g' | 'pg' | 'pg-13';
-
 export interface Gif {
     id: string;
     title: string;
-    description?: string;
     url: string;
+    detailUrl?: string;
+    altText?: string;
     username?: string;
     tags: string[];
     rating: GifRating;
